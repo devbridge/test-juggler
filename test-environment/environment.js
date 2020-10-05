2a7b77bc9030e140cf0e1ca01558f17b8d939cf5
DS\maciuliv <vaidas.maciulis@cdk.com>
2020-10-05 18:08:28 +0300

    Added captureBrowserConsoleLogs setting to framework.config

--- test-environment/environment.js
+++ test-environment/environment.js
@@ -18,27 +18,31 @@ class CustomEnvironment extends PuppeteerEnvironment {
 
     async pageSetup(page) {
         page.setDefaultTimeout(config.defaultTimeout);
-        const chalk = require("chalk");
-        page
-            .on("console", msg => {
-                const type = msg.type().substr(0, 3).toUpperCase();
-                const colors = {
-                    LOG: text => text,
-                    ERR: chalk.red,
-                    WAR: chalk.yellow,
-                    INF: chalk.cyan
-                };
-                const color = colors[type] || chalk.blue;
-                console.log(color(`${type} ${msg.text()}`));
-            })
-            .on("pageerror", ({ message }) => console.log(chalk.red(message)))
-            .on("response", response => {
-                if (response.status() > 399)
-                {
-                    console.log(chalk.red(`${response.status()} ${response.url()}`));
-                }})
-            .on("requestfailed", request =>
-                console.log(chalk.magenta(`${request.failure().errorText} ${request.url()}`)));
+        
+        if (config.captureBrowserConsoleLogs) {
+            const chalk = require("chalk");
+            page
+                .on("console", msg => {
+                    const type = msg.type().substr(0, 3).toUpperCase();
+                    const colors = {
+                        LOG: text => text,
+                        ERR: chalk.red,
+                        WAR: chalk.yellow,
+                        INF: chalk.cyan
+                    };
+                    const color = colors[type] || chalk.blue;
+                    console.log(color(`${type} ${msg.text()}`));
+                })
+                .on("pageerror", ({ message }) => console.log(chalk.red(message)))
+                .on("response", response => {
+                    if (response.status() > 399) {
+                        console.log(chalk.red(`${response.status()} ${response.url()}`));
+                    }
+                })
+                .on("requestfailed", request =>
+                    console.log(chalk.magenta(`${request.failure().errorText} ${request.url()}`)));
+        }
+        
         if (config.useTracing) {
             const targetDir = "./logs/Timelines";
             fs.mkdirSync(targetDir, { recursive: true });
