620be4309e57fee8d1727b16557c2ef929183e43
ernestas-zekas <ernestas.zekas@gmail.com>
2021-02-04 15:50:59 +0200

    #85 - Added setting exitOnPageError with value false to avoid crashing tests on webkit; - Refactored tests with new demo site.

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -3,17 +3,13 @@ module.exports = {
     // Describe which browsers we want to run
 
     browsers: ["chromium", "firefox", "webkit"],
-
+    exitOnPageError: false,
     launchOptions: {
-
         // If we want to run browsers in headless mode or not,
-
-        headless: false,
+        headless: true,
 
         // If we want to have opened devtools from start
-
-        devtools: false,
-
+        devtools: false
     }
 
 };
