ff1eb532b2295cc5d0bb531756e63ae0fabfd34f
vmaciulis <vaidas.maciulis@devbridge.com>
2021-05-13 15:37:33 +0300

    Restructure source for TS and add tsconfig.json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { default as Element } from "./framework/Element";
-export { default as Helpers } from "./framework/helpers";
-export { default as ApiHelpers } from "./framework/apiHelpers";
-export { default as Interceptor } from "./framework/interceptor";
+export { default as Element } from "./src/framework/Element";
+export { default as Helpers } from "./src/framework/helpers";
+export { default as ApiHelpers } from "./src/framework/apiHelpers";
+export { default as Interceptor } from "./src/framework/interceptor";
