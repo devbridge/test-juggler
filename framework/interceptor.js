047152ea52954c3a4f0ab1e9a4be21b46ccb6ae2
Jelena Rybakova <jelena.rybakova@devbridge.com>
2021-05-28 09:29:55 +0300

    [20] Updating interceptor class and tests to contain page

--- framework/interceptor.js
+++ framework/interceptor.js
@@ -1,14 +1,19 @@
 const fs = require("fs");
 
-class Interceptor {
+export default class Interceptor {
+    
+    constructor(interceptorPage = page) {
+        this.page = interceptorPage
+    }
+
     async takeScreenshot(filename) {
         const targetDir = `./logs/${jasmine["currentSuite"].fullName}/${jasmine["currentTest"].description}`;
         fs.mkdirSync(targetDir, { recursive: true });
-        await page.screenshot({ path: `${targetDir}/${filename || Date.now()}.png` });
+        await this.page.screenshot({ path: `${targetDir}/${filename || Date.now()}.png` });
     }
 
     async abortRequests(requestUrlFragment = "**") {
-        await page.route(requestUrlFragment, route => {
+        await this.page.route(requestUrlFragment, route => {
             route.abort();
             console.log(`Aborted request Url: '${route.request().url()}'`);
         } );
@@ -17,8 +22,8 @@ class Interceptor {
     async abortRequestsAfterAction(action, requestUrlFragment = "", waitDuration = 500) {
         await this.abortRequests(requestUrlFragment);
         await action;
-        await page.waitForTimeout(waitDuration);
-        await page.unroute(requestUrlFragment);
+        await this.page.waitForTimeout(waitDuration);
+        await this.page.unroute(requestUrlFragment);
     }
 
     async getAllRequestsData(action) {
@@ -26,25 +31,24 @@ class Interceptor {
         const requestLogger = request => {
             requestsData.push(request);
         };
-        page.on("request", requestLogger);
+        this.page.on("request", requestLogger);
         await action;
-        page.removeListener("request", requestLogger);
+        this.page.removeListener("request", requestLogger);
         return requestsData;
     }
 
     async waitForRequestAfterAction(action, requestUrlFragment = "") {
-        const requestAferAction = await page.waitForRequest(request => request.url().indexOf(requestUrlFragment) > -1);
+        const requestAferAction = await this.page.waitForRequest(request => request.url().indexOf(requestUrlFragment) > -1);
         await action;
         console.log(`Url: '${requestAferAction.url()}'`);
         return requestAferAction;
     }
 
     async waitForResponseAfterAction(action, responsetUrlFragment = "") {
-        const responseAferAction = await page.waitForResponse(response => response.url().indexOf(responsetUrlFragment) > -1);
+        const responseAferAction = await this.page.waitForResponse(response => response.url().indexOf(responsetUrlFragment) > -1);
         await action;
         console.log(`Url: '${responseAferAction.url()}'`);
         return responseAferAction;
     }
 }
 
-export default new Interceptor();
